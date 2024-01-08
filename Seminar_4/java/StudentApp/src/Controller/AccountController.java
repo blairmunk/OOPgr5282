4fedb4a586ec46cd048cbfccb685d73be9c7973f
Alexander Lemport <lemportpro@mail.ru>
2024-01-08 14:49:31 +0300

    Added some docs

--- Seminar_4/java/StudentApp/src/Controller/AccountController.java
+++ Seminar_4/java/StudentApp/src/Controller/AccountController.java
@@ -5,10 +5,22 @@ import Domen.Person;
 
 
 public class AccountController {
+    /**
+     * * Metod for paying salary
+     * @param <T> type of person (teacher)
+     * @param person person
+     * @param salary how much
+     */
     public <T extends Teacher> void paySalary(T person, int salary){
         System.out.println(person.getName() + " получил зарплату " + salary +  " рублей");
     }
 
+    /**
+     * Metod for calculating persons' average age
+     * @param <T> type of person: student, Employee, teacher
+     * @param persons list of persons
+     * @return average age of persons
+     */
     public static <T> double averageAge(List<T> persons) {
         int totalAge = 0;
         for (T person : persons) {
